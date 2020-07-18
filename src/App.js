3bad62d30965353f7c4b2178ac8791395e74f545
Fernando Servín <fernando.servin@kavak.com>
2020-07-18 18:27:32 -0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 // import { Counter } from './features/counter/Counter';
+
 import { Login } from './auth/Login';
+import Nav from './Nav';
+
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container is-fluid">
-        <Login />
-    </div>
-    // <Counter />
+    <Router>
+      {/* <Counter /> */}
+      <div className="container is-fluid">
+          
+          <Nav></Nav>
+          <Login />
+
+      </div>
+
+    </Router>
 
   );
 }
